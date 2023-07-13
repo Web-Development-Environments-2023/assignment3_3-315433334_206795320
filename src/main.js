e2acc1c18328bb885387fcfc525e1ee3531484c9
assaf7070 <76042716+assaf7070@users.noreply.github.com>
2023-07-13 11:47:47 +0300

    test commit

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ axios.interceptors.request.use(
     return config;
   },
   function(error) {
-    // Do something with request error
+    // Do something with request errorrrr
     return Promise.reject(error);
   }
 );
